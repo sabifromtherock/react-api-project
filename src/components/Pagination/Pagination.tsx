5303f0efa8262df9ef1112cf6a36b95e72a72f8f
sabifromtherock <szabolcs.sziklai@protonmail.com>
2023-08-22 18:45:17 +0100

    added Pagination component

---
+++ src/components/Pagination/Pagination.tsx
@@ -0,0 +1,74 @@
+import "./Pagination.scss";
+import rightArrow from "../../assets/images/right-arrow.png";
+import leftArrow from "../../assets/images/left-arrow.png";
+import rightDoubleArrow from "../../assets/images/right-arrow-double.png";
+import leftDoubleArrow from "../../assets/images/left-arrow-double.png";
+
+type PaginationProps = {
+  currentPage: number;
+  setCurrentPage: (currentPage: number) => void;
+  lastPage: number;
+};
+
+const Pagination = ({
+  currentPage,
+  setCurrentPage,
+  lastPage,
+}: PaginationProps) => {
+  const handlePageChange = (newPage: number) => {
+    setCurrentPage(newPage);
+  };
+
+  return (
+    <div className="pagination">
+      {currentPage > 2 && (
+        <img
+          className="pagination__image"
+          src={leftDoubleArrow}
+          alt=""
+          onClick={() => handlePageChange((currentPage = 1))}
+        />
+      )}
+      {currentPage > 1 && (
+        <img
+          className="pagination__image"
+          src={leftArrow}
+          alt=""
+          onClick={() => handlePageChange(currentPage - 1)}
+        />
+      )}
+      {currentPage === 1 ? null : currentPage === 2 ? (
+        <span> 1 </span>
+      ) : (
+        <span> 1 ... </span>
+      )}
+      <span>
+        <strong>{` Page ${currentPage} `}</strong>
+      </span>
+
+      {currentPage === lastPage ? null : currentPage === lastPage - 1 ? (
+        <span> {lastPage} </span>
+      ) : (
+        <span> ... {lastPage} </span>
+      )}
+      {currentPage !== lastPage && (
+        <img
+          className="pagination__image"
+          src={rightArrow}
+          alt=""
+          onClick={() => handlePageChange(currentPage + 1)}
+        />
+      )}
+      {currentPage < lastPage - 1 && (
+        <img
+          className="pagination__image"
+          src={rightDoubleArrow}
+          alt=""
+          onClick={() => handlePageChange((currentPage = lastPage))}
+        />
+      )}
+    </div>
+  );
+};
+
+export default Pagination;
--- src/containers/BeerContainer/BeerContainer.tsx
+++ src/containers/BeerContainer/BeerContainer.tsx
@@ -1,24 +1,41 @@
 import "./BeerContainer.scss";
 import BeerCard from "../../components/BeerCard/BeerCard";
 import { Beer } from "../../types/Beer";
+import Pagination from "../../components/Pagination/Pagination";
+import { useState } from "react";
 
 type BeerContainerProps = {
   beers: Beer[];
 };
 
 const BeerContainer = ({ beers }: BeerContainerProps) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const currentBeers = beers.slice(currentPage, currentPage + 10);
+
   return (
     <div className="beer-container">
-      {beers.map((beer) => {
-        return (
-          <BeerCard
-            key={beer.id}
-            url={beer.image_url}
-            beer={beer}
-            variable={""}
-          />
-        );
-      })}
+      <Pagination
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+        lastPage={Math.ceil(beers.length / 10)}
+      />
+      <div className="beer-container__beers">
+        {currentBeers.map((beer) => {
+          return (
+            <BeerCard
+              key={beer.id}
+              url={beer.image_url}
+              beer={beer}
+              variable={""}
+            />
+          );
+        })}
+      </div>
+      <Pagination
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+        lastPage={Math.ceil(beers.length / 10)}
+      />
     </div>
   );
 };
