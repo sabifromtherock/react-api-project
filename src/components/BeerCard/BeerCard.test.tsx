819ca7ddd1c4392d2b6c123d29a09dfb2cfc7a6c
sabifromtherock <szabolcs.sziklai@protonmail.com>
2023-08-24 15:11:07 +0100

    added BeerCard test

---
+++ src/components/BeerCard/BeerCard.test.tsx
@@ -0,0 +1,44 @@
+import { render, screen } from "@testing-library/react";
+import BeerCard from "./BeerCard";
+import beers from "../../data/beers";
+import { BrowserRouter } from "react-router-dom";
+import userEvent from "@testing-library/user-event";
+
+it("should render the BeerCard", () => {
+  // arrange
+  render(
+    <BrowserRouter>
+      <BeerCard beer={beers[0]} variant="" />
+    </BrowserRouter>
+  );
+
+  // act
+  const image = screen.getByRole("img");
+  const name = screen.getByText("Name:");
+  const firstBrewed = screen.getByText("First Brewed:");
+  const abv = screen.getByText("ABV:");
+  const ph = screen.getByText("pH:");
+
+  // assert
+  expect(image).toBeInTheDocument();
+  expect(name).toBeInTheDocument();
+  expect(firstBrewed).toBeInTheDocument();
+  expect(abv).toBeInTheDocument();
+  expect(ph).toBeInTheDocument();
+});
+
+it("should render url when BeerCard is clicked ", async () => {
+  // arrange
+  render(
+    <BrowserRouter>
+      <BeerCard beer={beers[0]} variant="" />
+    </BrowserRouter>
+  );
+
+  // act
+  const beerCard = screen.getByRole("link");
+  await userEvent.click(beerCard);
+
+  //assert
+  expect(beerCard).toHaveAttribute("href", `/beer/${beers[0].id}`);
+});
